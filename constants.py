ccfc7e61d966d434e1cf0c713c76360e168fbbf9
edfarialand <105104922+edfarialand@users.noreply.github.com>
2024-08-17 03:24:02 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 GDOCS_URL = "https://docs.google.com/spreadsheets/d/"
 CSV_ENDING = "/export?format=csv&gid="
-ATLAS_ID = "1pu4Adxq4MGB6Qour0k__4gBdgnggWRoSVYnJUKgxzEw"
+ATLAS_ID = "1tJQ-WdQZc91cCw0ExvPe6-lOkRuHVRbIsj4D2nNwuTo"
 
 HANGGROUP_URL = "https://us7.campaign-archive.com/feed?u=c436b270f72b9fd53ce62e913&id=c249be35a0"
 
@@ -12,7 +12,7 @@ class AtlasSheets(Enum):
     """Maps sheet names to google doc ID"""
     GENERAL_INFO_GRADING = 1453313541
     NEW_IN_BOX = 677728473
-    IPHONE_USED = 0
+    IPHONE_USED = 1216102336
     PARTS = 724685250
     APPLE_WATCH = 1323917402
     MACBOOKS = 2071430745
